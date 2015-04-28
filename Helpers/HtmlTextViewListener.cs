e79803c2c487082b10fb0febdf7419f3ba9726cf
Rion Williams <rionmonster@gmail.com>
2015-04-28 13:43:09 +0000

    Updated Glyphs; Improved Performance
    Created a TextViewListener to trigger Glyph generation when a new HTML
    page was opened (as opposed to handling that lazily); Updated all Glyph
    names to use a library-specific prefix to ensure they were unique (as
    they were now being stored in a dictionary). General housekeeping
    changes.

---
+++ Helpers/HtmlTextViewListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace Glyphfriend.Helpers
+{
+    [Export(typeof(IVsTextViewCreationListener))]
+    [ContentType("htmlx")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    class HtmlTextViewListener : IVsTextViewCreationListener
+    {
+        // This will be triggered when an HTML document is opened
+        public void VsTextViewCreated(IVsTextView textViewAdapter)
+        {
+            // If the Glyphs haven't been loaded, load them
+            if(GlyphfriendPackage.Glyphs == null)
+            {
+                GlyphfriendPackage.LoadGlyphs();
+            }
+        }
+    }
+}
