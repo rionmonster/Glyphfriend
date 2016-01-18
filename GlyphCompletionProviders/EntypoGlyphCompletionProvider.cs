a3e26d78dad53764dfb7c4e5c5bf1d09e087f8c0
Rion Williams <rionmonster@gmail.com>
2016-01-18 16:22:24 -0600

    Added Entypo Provider and associated Glyphs
    Added Entypo Support including provider and associated Glyphs (per Issue
    #30)

---
+++ GlyphCompletionProviders/EntypoGlyphCompletionProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.CSS.Editor.Completion;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Glyphfriend.GlyphCompletionProviders
+{
+    [Export(typeof(ICssCompletionEntryGlyphProvider))]
+    [Name("Glyphfriend Entypo")]
+    class EntypoGlyphCompletionEntryGlyphProvider : ICssCompletionEntryGlyphProvider
+    {
+        // Define a Regular Expression check for matches from this library
+        private static Regex _regex = new Regex(@"^entypo?(\.min)?\.css$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        public ImageSource GetCompletionGlyph(string entryName, Uri sourceUri, CssNameType nameType)
+        {
+            // If the source Uri for the image is null, ignore it
+            if (sourceUri == null) { return null; }
+            // Get the file path of the source being used
+            string filename = Path.GetFileName(sourceUri.ToString()).Trim();
+            // Determine if this matches our filename
+            if (_regex.IsMatch(filename))
+            {
+                if (GlyphfriendPackage.Glyphs.ContainsKey(entryName))
+                {
+                    return GlyphfriendPackage.Glyphs[entryName];
+                }
+                return GlyphfriendPackage.Glyphs["icon-default"];
+            }
+
+            return null;
+        }
+    }
+}
