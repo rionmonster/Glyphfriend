5012823c47a8671ea27351b4f4d8ca599081e100
Rion Williams <rionmonster@gmail.com>
2017-01-10 21:46:31 +0000

    Added VSPackage to Glyphfriend.Core
    Added VSPackage to handle loading / deserializing the available binary
    mapping file. Wired up the package to initialize on Visual Studio load.

---
+++ src/Glyphfriend.Core/VSPackage.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using ProtoBuf;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Glyphfriend
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(UIContextGuids80.SolutionExists)]
+    [ProvideAutoLoad(UIContextGuids80.NoSolution)]
+    // TODO: Determine if we can target a "later" context (i.e. load when an HTML file is opened
+    // as opposed to loading when Visual Studio is launched)
+    public sealed class VSPackage : Package
+    {
+        internal static Dictionary<string, ImageSource> Glyphs { get; private set; }
+        internal static string AssemblyLocation => Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+        public VSPackage()
+        {
+            DeserializeGlyphsFromBinary();
+        }
+
+        private void DeserializeGlyphsFromBinary()
+        {
+            var binaryGlyphDictionary = DeserializeBinaryGlyphs();
+            Glyphs = ConvertBinaryGlyphDictionaryToGlyphDictionary(binaryGlyphDictionary);
+        }
+
+        private Dictionary<string, byte[]> DeserializeBinaryGlyphs()
+        {
+            var binaryPath = Path.Combine(AssemblyLocation, "glyphs.bin");
+            using (var fs = File.Open(binaryPath, FileMode.Open))
+            {
+                return Serializer.Deserialize<Dictionary<string, byte[]>>(fs);
+            }
+        }
+
+        private Dictionary<string, ImageSource> ConvertBinaryGlyphDictionaryToGlyphDictionary(Dictionary<string, byte[]> dictionary)
+        {
+            return dictionary.ToDictionary(k => k.Key, v => BytesToImage(v.Value));
+        }
+
+        private ImageSource BytesToImage(byte[] imageData)
+        {
+            var image = new BitmapImage();
+            using (var ms = new MemoryStream(imageData))
+            {
+                image.BeginInit();
+                image.StreamSource = ms;
+                // This is required for images to be loaded by Visual Studio on-demand
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+            }
+            return image as ImageSource;
+        }
+    }
+}
