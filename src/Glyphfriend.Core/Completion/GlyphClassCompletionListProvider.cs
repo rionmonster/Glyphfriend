6349c4148d89f6e72ab6a9e265f084c44621611d
Rion Williams <rionmonster@gmail.com>
2017-01-10 21:41:43 +0000

    Added CompletionProvider Classes
    Added CompletionProvider classes (both abstract and implementation) to
    handle serving glyphs within the extension.

---
+++ src/Glyphfriend.Core/Completion/GlyphClassCompletionListProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Html.Editor.Completion;
+using Microsoft.Html.Editor.Completion.Def;
+using Microsoft.VisualStudio.Utilities;
+using System.Collections.Generic;
+
+namespace Glyphfriend
+{
+    [HtmlCompletionProvider(CompletionTypes.Values, "*", "class")]
+    [ContentType("htmlx")]
+    class GlyphClassCompletionListProvider : BaseClassCompletionProvider
+    {
+        public override string CompletionType
+        {
+            get { return CompletionTypes.Values; }
+        }
+
+        public override IList<HtmlCompletion> GetEntries(HtmlCompletionContext context)
+        {
+            var completionItems = new List<HtmlCompletion>();
+            foreach (var glyph in VSPackage.Glyphs)
+            {
+                completionItems.Add(CreateItem(glyph.Key, glyph.Value, context.Session));
+            }
+            return completionItems;
+        }
+    }
+}
