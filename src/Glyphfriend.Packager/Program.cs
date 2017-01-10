1e52daf69d2a8ba6d5a7d9cd47702401332ad9c3
Rion Williams <rionmonster@gmail.com>
2017-01-10 22:03:06 +0000

    Added Protobuf; Serialization Logic
    Added reference to Protobuf; Wired up basic serialization logic to
    handle traversing Glyphs directory.

--- src/Glyphfriend.Packager/Program.cs
+++ src/Glyphfriend.Packager/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
+﻿using ProtoBuf;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Glyphfriend.Packager
 {
@@ -10,6 +8,34 @@ namespace Glyphfriend.Packager
     {
         static void Main(string[] args)
         {
+            GenerateBinaryGlyphsFile();
+        }
+
+        private static void GenerateBinaryGlyphsFile()
+        {
+            var glyphDictionary = ConvertGlyphsToDictionary();
+            BinarySerializeGlyphsToFile(glyphDictionary);
+        }
+
+        private static Dictionary<string, byte[]> ConvertGlyphsToDictionary()
+        {
+            var glyphs = new Dictionary<string, byte[]>();
+            var glyphDirectory = new DirectoryInfo("../../Glyphs");
+            foreach (var glyph in glyphDirectory.EnumerateFiles("*.png", SearchOption.AllDirectories))
+            {
+                var glyphContent = StreamHelpers.ReadFully(new FileStream(glyph.FullName, FileMode.Open));
+                glyphs.Add(Path.GetFileNameWithoutExtension(glyph.Name), glyphContent);
+            }
+            return glyphs;
+        }
+
+        private static void BinarySerializeGlyphsToFile(Dictionary<string, byte[]> glyphs)
+        {
+            using (var ms = new FileStream("../../Binary/glyphs.bin", FileMode.Create))
+            {
+                Serializer.Serialize(ms, glyphs);
+                ms.Flush();
+            }
         }
     }
 }
