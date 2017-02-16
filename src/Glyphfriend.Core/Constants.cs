ec7b1b38bbcaffa7ae8848b53727bf2880005884
Rion Williams <rionmonster@gmail.com>
2017-02-16 16:50:00 -0600

    Added Constants.cs
    Added Constants.cs to Glyphfriend.Core in order to have a single
    location to store Guids and any other future values that might arise in
    the extension within a single place.

---
+++ src/Glyphfriend.Core/Constants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Glyphfriend
+{
+    class Constants
+    {
+        public const string HtmlFileLoadedContext = "21F5568E-A5DE-4821-AF39-F4F1049BB9CF";
+        public static readonly Guid PackageGuid = new Guid("21F5568E-A5DE-4821-AF39-F4F1049BB9CF");
+    }
+}
