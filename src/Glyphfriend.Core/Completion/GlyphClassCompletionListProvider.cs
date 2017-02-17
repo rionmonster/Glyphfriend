b97f2c1b9e730dc2967cdb54184b3da1bd5d2810
Rion Williams <rionmonster@gmail.com>
2017-02-17 09:15:55 -0600

    Updated GlyphClassCompletionListProvider.cs
    Removed private Package variable as it will be unnecessary to try and
    persist it.

--- src/Glyphfriend.Core/Completion/GlyphClassCompletionListProvider.cs
+++ src/Glyphfriend.Core/Completion/GlyphClassCompletionListProvider.cs
@@ -17,17 +17,17 @@ namespace Glyphfriend
         [Import]
         protected SVsServiceProvider GlobalServiceProvider { get;  private set; }
         public override string CompletionType => CompletionTypes.Values;
-        private VSPackage _package;
 
         public override IList<HtmlCompletion> GetEntries(HtmlCompletionContext context)
         {
-            _package = (VSPackage)EnsurePackageLoaded();
-            if(_package == null)
+            VSPackage package = (VSPackage)EnsurePackageLoaded();
+            if(package == null)
             {
                 return new List<HtmlCompletion>();
             }
+
             var glyphCompletionItems = new List<HtmlCompletion>();
-            foreach (var glyph in _package.Glyphs)
+            foreach (var glyph in package.Glyphs)
             {
                 glyphCompletionItems.Add(CreateItem(glyph.Key, glyph.Value, context.Session));
             }
@@ -36,12 +36,6 @@ namespace Glyphfriend
 
         private IVsPackage EnsurePackageLoaded()
         {
-            // Don't try to load it again
-            if(_package != null)
-            {
-                return _package;
-            }
-
             IVsPackage package;
             // If the package failed to load or is null, explicitly load it
             if (!ErrorHandler.Succeeded(GlobalServiceProvider.GetShell().IsPackageLoaded(Constants.PackageGuid, out package)) || package == null)
