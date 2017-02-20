83ca323b98c179158044e6b301a6fe7b147801b1
Rion Williams <rionmonster@gmail.com>
2017-02-20 16:57:50 -0600

    Removed Unnecessary Resources
    Removed several unnecessary and unused resourecs from the
    ToggleLibraryCommand scaffolding. Changed the name of the HTML context
    menu to something more meaningful (Menu.cs -> GlyphfriendHtmlMenu.ct)

--- src/Glyphfriend.VS2017/ToggleLibraryCommandPackage.cs
+++ src/Glyphfriend.VS2017/ToggleLibraryCommandPackage.cs
@@ -8,7 +8,7 @@ namespace Glyphfriend
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideMenuResource("GlyphfriendHtmlMenu.ctmenu", 1)]
     [Guid(ToggleLibraryCommandPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
     [ProvideAutoLoad(PackageGuidString)]
